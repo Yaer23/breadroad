6d3551f0a0bdbf8986ab64ce04be2074fed33436
Yaer23 <770631546@qq.com>
2018-11-12 16:36:18 +0800

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'breadRoad',
+      script    : './service/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'WEB',
+      script    : 'web.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'Lydia',
+      host : '132.232.124.245',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Yaer23/breadroad.git',
+      path : '/www/breadRoad',
+      "ssh_options": "StrictHostKeyChecking=no",
+      'post-deploy' : 'npm pull && ./run.sh && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
