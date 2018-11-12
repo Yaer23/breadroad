b5462c31be74aac666b8022eb4867e5578c1b78e
Yaer23 <770631546@qq.com>
2018-11-12 17:10:28 +0800

    modified ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,24 +3,24 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
 
     // First application
     {
-      name      : 'breadRoad',
-      script    : './service/index.js',
+      name: 'breadRoad',
+      script: './service/index.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production'
       }
     },
 
     // Second application
     {
-      name      : 'WEB',
-      script    : 'web.js'
+      name: 'WEB',
+      script: 'web.js'
     }
   ],
 
@@ -28,15 +28,15 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'Lydia',
-      host : '132.232.124.245',
-      ref  : 'origin/master',
-      repo : 'git@github.com:Yaer23/breadroad.git',
-      path : '/www/breadRoad',
+  deploy: {
+    production: {
+      user: 'Lydia',
+      host: '132.232.124.245',
+      ref: 'origin/master',
+      repo: 'git@github.com:Yaer23/breadroad.git',
+      path: '/www/breadRoad',
       "ssh_options": "StrictHostKeyChecking=no",
-      'post-deploy' : 'git pull && npm run build && pm2 reload ecosystem.config.js --env production'
+      'post-deploy': 'git pull && npm run build && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
