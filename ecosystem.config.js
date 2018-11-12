7483fa59ee4b8a7f1e04f4062845f7531ddcc2a0
Yaer23 <770631546@qq.com>
2018-11-12 16:43:32 +0800

    modified ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,7 +36,7 @@ module.exports = {
       repo : 'git@github.com:Yaer23/breadroad.git',
       path : '/www/breadRoad',
       "ssh_options": "StrictHostKeyChecking=no",
-      'post-deploy' : 'npm pull && ./run.sh && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm pull && npm install && npm run build && cd ./service && npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
       user : 'node',
