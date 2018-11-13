cef2413e5a7534e96e072b35a18ecc62408f681f
Yaer23 <770631546@qq.com>
2018-11-13 15:55:45 +0800

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 
-npm install --registry=https://registry.npm.taobao.org && npm run build && cd ./service && npm install --registry=https://registry.npm.taobao.org && cd .. && mkdir ./service/public && mv ./dist/index.html ./service/public/index.html && mv ./dist/static/ ./service/public/static/ && rm -rf ./dist
+cnpm install && npm run build && cd ./service && cnpm install && cd .. && mkdir ./service/public && mv ./dist/index.html ./service/public/index.html && mv ./dist/static/ ./service/public/static/ && rm -rf ./dist
